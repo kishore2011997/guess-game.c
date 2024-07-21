5cae154388fc2d2375cfc60106dc624179aa52b9
kishore2011997 <parthiban.kishore01@gmail.com>
2024-07-21 19:08:43 +0530

    Add files via upload
    guessing game in c-language between computer and me play the game.

---
+++ guess.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+
+    int secret_number;
+    int guess_number;
+    printf("Welcome to the game\n");
+    printf("Enter the guess number\n");
+    scanf("%d",&guess_number);
+    printf("Enter ")
+    if(secret_number==guess_number)
+    {
+        printf("You win the game\n");
+    }
+    else
+    {
+        printf("You are lost");
+    }
+    return 0;
+}
